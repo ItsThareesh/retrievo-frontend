50a07e0c76b70eb1fdc4045dbd03506b67b796be
ItsThareesh <developer.thareesh@gmail.com>
2026-01-25 12:51:48 +0530

    refactor: remove dotenv configuration
    - move .env file within repo dir

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 import type { NextConfig } from "next";
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({
-  path: path.resolve(__dirname, "..", ".env.frontend"),
-});
 
 const nextConfig: NextConfig = {
   output: "standalone",
